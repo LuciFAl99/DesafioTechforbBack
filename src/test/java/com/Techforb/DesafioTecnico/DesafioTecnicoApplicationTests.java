4766f5f224484a53e987cc6fc4568cb3b3020122
Lucila Alochis <lucila.alochis1999@gmail.com>
2023-11-19 08:44:23 -0300

    Inicialización de proyecto en Spring

---
+++ src/test/java/com/Techforb/DesafioTecnico/DesafioTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Techforb.DesafioTecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
